1536a747f8f5c04df8221ebe1566e8690adc82f6
Yaroslav Zhgilev <jarslavzhgilev@rambler.ru>
2019-11-25 13:06:28 +0300

    modification main

--- main/main/main.cpp
+++ main/main/main.cpp
@@ -1,41 +1,42 @@
 #include "stdafx.h"
 #include <iostream>
+#include "function1.h"
 using namespace std;
 
 int main(int argc, char* argv[])
 	{
-		 int count; // переменная для выбора в switch
-		 double a, b; // переменные для хранения операндов
+		 int count; 
+		 double a, b; 
 			cout << "Vvedite pervoe chislo: "<<endl;
 			cin >> a;
 			cout << "Vvedite vtoroe chislo: "<<endl;
 			cin >> b;
 			cout << "Vibirite deistvie: 1-clojenie; 2-vichitanie; 3-ymnojenie; 4-delenie: "<<endl;
 			 cin >> count;
- switch (count) // начало оператора switch
+ switch (count) 
  {
 
-case 1: // если count = 1
+case 1: 
  {
-	 cout<<"Vremenno nedostupno"<<endl;
+	 cout<<funcadd(a,b)<<endl;
  break;
  }
- case 2: // если count = 2
+ case 2: 
  {
-	 cout<<"Vremenno nedostupno"<<endl;
+	 cout<<funcsub(a,b)<<endl;
  break;
  }
- case 3: // если count = 3
+ case 3: 
  {
-	 cout<<"Vremenno nedostupno"<<endl;
+	 cout<<funcmulti(a,b)<<endl;
  break;
  }
- case 4: // если count = 4
+ case 4: 
  {
-	 cout<<"Vremenno nedostupno"<<endl;
+	 cout<<funcdiv(a,b)<<endl;
  break;
  }
- default: // если count равно любому другому значению
+ default: 
  cout << "Nepravilni vvod" << endl;
  }
  system("pause");
