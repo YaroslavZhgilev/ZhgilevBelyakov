1f7d843c422c0af77fe8aa476fe72fbbfc7d7ef0
Yaroslav Zhgilev <jarslavzhgilev@rambler.ru>
2019-11-25 12:41:33 +0300

    add function division and subtraction

---
+++ main/main/function1.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h" 
+#include "function1.h"
+	
+double funcdiv(double a,double b)
+	{
+		return a/b;
+}
+double funcsub(double a,double b)
+{
+	return a-b;
+}
+
---
+++ main/main/function1.h
@@ -0,0 +1,5 @@
+#ifndef __FUNCTION1_H__ 
+#define __FUNCTION1_H__  
+double funcdiv(double,double);
+double funcsub(double,double);
+#endif
